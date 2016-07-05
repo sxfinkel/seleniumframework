ecae1503f7c3c75649830ade3e5f17b562d67956
Susan Barrows <susan.finkel@outlook.com>
2016-07-05 10:11:18 -0700

    Rename Constants.java to Constants2.java

---
+++ Constants2.java
@@ -0,0 +1,23 @@
+package com.aexp.wsgcat.seleniumframework.constants;
+
+/**
+ * Created by cteix4 on 9/1/2015.
+ */src/main/java/com/aexp/qa/seleniumframework/constants/Constants.java
+ */
+public final class Constants {
+
+    /** Time out in seconds used in the steps for waiting for an element. **/
+    public static final int MAXIMUM_WAIT_TIME = 60;
+
+    /** Wait Time in milliseconds used in the steps for waiting for an element. **/
+    public static final int SYNC_WAIT_TIME = 2000;
+
+    /** Screenshot sub folder. **/
+    public static final String SCREENSHOT_SUBFOLDER = "screenshot";
+    /**
+     * Prevents from constructing objects of this class, by declaring this private constructor.
+     */
+    private Constants() {
+    }
+}
+
